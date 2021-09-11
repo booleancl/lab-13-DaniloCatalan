06216a1b1b638dc364bbce59ff9f12b405b47bb2
DaniloCatalan <danilo.e.catalan@gmail.com>
2021-09-10 21:47:03 -0300

    upd

--- nodejs-modules/02_child_process.test.js
+++ nodejs-modules/02_child_process.test.js
@@ -4,9 +4,9 @@ describe('spwan command execution',  () => {
 
   it('get data from git config command', async () => {
     const result = await getGitUserData()
-    console.log(result)
-    expect(result).toMatch('DaniloCatalan')
-    expect(result).toMatch('danilo.e.catalan@gmail.com')
+    // console.log(result)
+    expect(result).toMatch('user.name')
+    expect(result).toMatch('user.email')
   })
   
 })
